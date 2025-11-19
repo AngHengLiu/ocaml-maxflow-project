d17ea42251e918a7023146dfff52a8ffda3ca4f9
Carole <caroleanh04@gmail.com>
2025-11-19 11:40:47 +0100

    implement gmap

--- src/tools.ml
+++ src/tools.ml
@@ -3,5 +3,12 @@ open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
 let clone_nodes _gr = assert false
-let gmap _gr _f = assert false
+let gmap gr f = 
+    let aux = function 
+        | None -> ()
+        | Some a -> a.label = f a.label
+    in 
+        Graph.e_iter gr aux
+;;
+
 (* Replace _gr and _f by gr and f when you start writing the real function. *)
