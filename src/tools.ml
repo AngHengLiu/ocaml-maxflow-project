edbd6f0ae5c2eb6b8d736b5a74e435cb354317f4
Beaugeois Carole <beaugeois@insa-20668>
2025-11-24 16:12:02 +0100

    implement add_arc

--- src/tools.ml
+++ src/tools.ml
@@ -3,12 +3,9 @@ open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
 let clone_nodes _gr = assert false
-let gmap gr f = 
-    let aux = function 
-        | None -> ()
-        | Some a -> a.label = f a.label
-    in 
-        Graph.e_iter gr aux
-;;
-
+let gmap gr f = Graph.e_fold gr (fun b a -> new_arc b {src = a.src; tgt = a.tgt; lbl = f a.lbl}) clone_nodes
 (* Replace _gr and _f by gr and f when you start writing the real function. *)
+let add_arc g id1 id2 n = 
+    match Graph.find_arc g id1 id2 with 
+    | None -> Graph.new_arc g {src = id1; tgt = id2; lbl = n}
+    | Some a -> Graph.new_arc g {src = a.src; tgt = a.tgt; lbl = (a.lbl + n)}
