42059a2235744ceeac67b3a31f87eda681e9b9f0
Angela Liu <angela.liu0303@gmail.com>
2025-11-24 16:17:55 +0100

    clone_nodes done

--- src/tools.ml
+++ src/tools.ml
@@ -2,7 +2,7 @@
 open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
-let clone_nodes _gr = assert false
+let clone_nodes gr = n_fold new_node empty_graph gr
 let gmap gr f = Graph.e_fold gr (fun b a -> new_arc b {src = a.src; tgt = a.tgt; lbl = f a.lbl}) clone_nodes
 (* Replace _gr and _f by gr and f when you start writing the real function. *)
 let add_arc g id1 id2 n = 
