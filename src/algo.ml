6173bf64762b687074ed120cbc4dfe94cb40dad3
Angela Liu <angela.liu0303@gmail.com>
2026-01-10 11:44:15 +0100

    Deleted a non-used function

--- src/algo.ml
+++ src/algo.ml
@@ -46,11 +46,6 @@ let search_flow_path graph src tgt =
     | None -> None 
     | Some fp -> Some (List.rev fp)
 
-(*let print_edge g = 
-  Printf.printf "\n";
-  e_iter g (fun arc -> Printf.printf "[ %d -> %d ]" arc.src arc.tgt);
-  Printf.printf "\n"*)
-
 (*Ford-Flukerson algo*)
 let rec ford_flukerson_loop graph src tgt = 
 
